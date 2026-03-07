381136be2caecd84821656257af3c76ef8345523
ivankush2000 <ivankush2000@gmail.com>
2026-03-08 03:29:04 +0800

    Create Address.py

---
+++ Lesson_3/Address.py
@@ -0,0 +1,7 @@
+class Address:
+    def __init__(self, indeks, city, street, hom, apartment):
+        self.indeks = indeks
+        self.city = city
+        self.street = street
+        self.hom = hom
+        self.apartment = apartment
